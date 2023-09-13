260509bafe51a4030cff8972f949134cb4c6e0b5
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-09-13 14:42:38 +0700

    name nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,18 @@ import { Fragment } from "react";
 
 const Header = () => {
 
-
+    let Links = [
+        {name: 'Home', link: '/'},
+        {name: 'News', link: '/'},
+        {name: 'Live cameras', link: '/'},
+        {name: 'Photos', link: '/'},
+        {name: 'Contacts', link: '/'},
+    ]
 
     return (
         <Fragment>
-        <div className='shadow-md w-full bg-[#1e202b]'>
-            <div className=' md:px-40 py-10 px-7'>
+        <div className='shadow-md w-full fixed top-0 left-0 bg-[#1e202b]'>
+            <div className='md:px-40 py-12 px-20 md:flex justify-between items-center'>
                 <div className="flex cursor-pointer items-center gap-4 ">
                     <img src="/src/assets/logo@2x.png" alt="logo" className=" w-16  " />
                     <div className="flex flex-col">
@@ -17,7 +23,17 @@ const Header = () => {
                         <span className="text-gray-300 text-xs">tagline goes here</span>
                     </div>
                 </div>
+                <ul className="md:flex pl-9 md:pl-0 gap-4">
+                {
+                    Links.map((link, index) => (
+                        <li key={index} className="text-slate-100 my-7 md:my-0 md:ml-8 hover:text-[#009ad8] py-1 px-5 border-[]  ">
+                            <a href="/">{link.name}</a>
+                        </li>))
+                }
+            </ul>
             </div>
+
+            
         </div>
         </Fragment>
     );
