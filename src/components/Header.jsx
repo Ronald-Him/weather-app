bbadcc970957bfc067ea9f1e9aee44535b465610
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-09-14 11:26:40 +0700

    change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -36,7 +36,7 @@ const Header = () => {
                 </div>
                     
 
-                <ul className={`md:flex pl-9 md:pl-0 gap-4 md:items-center md:pb-0 pb-12 absolute md:static bg-[#1e202b] md:z-auto z-[-1] left-0 w-full md:w-auto transistion-all duration-500 ease-in ${isOpen ? 'top-12' : 'top-[-490px]'} `}>
+                <ul className={`md:flex pl-9 md:pl-0 gap-4 md:items-center md:pb-0 pb-12 absolute md:static bg-[#1e202b] md:z-auto z-[1] left-0 w-full md:w-auto transistion-all duration-500 ease-in ${isOpen ? 'top-12' : 'top-[-490px]'} `}>
                 {
                     Links.map((link, index) => (
                         <li key={index} className="text-slate-100 button button-hover cursor-pointer">
