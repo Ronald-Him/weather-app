f5202d10a903616db1ab17dd402836cc7924b183
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-09-14 16:58:08 +0700

    change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,6 @@ function Home(){
     return (
         <Fragment>
             <div className="img-Background"></div>
-            <form action="#" className="relative text-cyan-400">
-                <input type="text" placeholder="hello" className="bg-[#1e202b] mb-[6rem]" />
-                <button type="submit" className="bg-[#039fd9] text-white">Find</button>
-            </form>
         </Fragment>
     )
 }
