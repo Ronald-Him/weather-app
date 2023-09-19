2e03608861a2bdc205c92dd55a9dc01e367e4143
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2023-09-19 13:35:19 +0700

    include pages inside app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import Header from './components/header'
 import Home from './pages/Home'
-
+import News from './pages/News'
+import LiveCameras from './pages/LiveCameras'
+import Photos from './pages/Photos'
+import Contact from './pages/Contact'
 function App() {
   return (
     <>
     <div className='App bg-[#1e202b] w-full h-screen'>
       <Header/> 
       <Home/>
+      <News/>
+      <LiveCameras/>
+      <Photos/>
+      <Contact/>
     </div>
 
     </>
